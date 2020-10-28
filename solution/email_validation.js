1e52d4b0848fe41a5ca6cf6592778d0c494fc528
Merve <gulineyel@gmail.com>
2020-10-28 20:23:08 +0300

    if-3

--- solution/email_validation.js
+++ solution/email_validation.js
@@ -4,7 +4,11 @@ function email_val(email) {
   }
   if (
     email.charAt(email.length - 4) != "." &&
-    
+    if (
+      email.charAt(email.length - 4) != "." &&
+      email.charAt(email.length - 3) != "."
+    ) {
+      console.log("Invalid . position at 4");
   }
 }
 
